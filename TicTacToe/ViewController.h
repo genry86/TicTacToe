8444d5c63b635e4c7f248faced61150098027caf
Genry <genry.vyaznikov@gmail.com>
2018-07-27 19:01:39 +0300

    Initial Commit

---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Genry on 7/27/18.
+//  Copyright © 2018 Genry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Genry on 7/27/18.
+//  Copyright © 2018 Genry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
